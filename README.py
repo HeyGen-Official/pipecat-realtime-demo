7040e2d008bbb877b0021a66e7588d3b4cc0a643
Eddy Kim <eddy@heygen.com>
2024-12-11 17:10:23 -0800

    initial pipecat demo implementation

---
+++ README.py
@@ -0,0 +1,17 @@
+# HeyGen Pipecat Demo
+
+## Setup
+Python 3.10 or higher
+
+```bash
+pip install -r requirements.txt
+```
+
+## Run
+
+```bash
+export ELEVENLABS_API_KEY=YOUR_ELEVENLABS_API_KEY
+export OPENAI_API_KEY=YOUR_OPENAI_API_KEY
+export DEEPGRAM_API_KEY=YOUR_DEEPGRAM_API_KEY
+python main.py
+```
